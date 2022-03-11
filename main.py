8b45fe0e8fd10ebc52ec0b0215bf2a399249a5f5
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-03-11 22:51:52 +0700

    komentar

---
+++ main.py
@@ -0,0 +1,18 @@
+#operator penugasan
+# =,+=,-=,*=,/=,%=,**=,//=
+
+nilai = 4
+nilai += 2
+print(nilai)
+nilai -= 1
+print(nilai)
+nilai *= 3
+print(nilai)
+nilai /= 3
+print(nilai)
+nilai %= 3
+print(nilai)
+nilai **= 3
+print(nilai)
+nilai //= 3
+print(nilai)
